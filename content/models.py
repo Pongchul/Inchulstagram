00625b961d036fb9d92514872de95002acf5514f
Pongchul <inchul7511@gmail.com>
2022-09-22 01:24:15 +0900

    inchulstagram

---
+++ content/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Feed(models.Model):
+    content = models.TextField()  # 글 내용
+    image = models.TextField()  # 피드 이미지
+    email = models.EmailField(default='')
+
+
+class Like(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    is_like = models.BooleanField(default=True)
+
+
+class Reply(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    reply_content = models.TextField()
+
+
+class Bookmark(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    is_marked = models.BooleanField(default=True)
